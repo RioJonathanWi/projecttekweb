c9bb56d0ba3a53fab2a92d944c685b68f81210d7
andrewchristoperr <andrewch03.ac@gmail.com>
2022-12-05 22:47:10 +0700

    a

---
+++ api/insertContactUs.php
@@ -0,0 +1,11 @@
+<?php
+    require "../connect.php";
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $msg = $_POST['msg'];
+
+    $query = "INSERT INTO contactus (nama, email, msg) VALUES ('$name', '$email', '$msg')";
+    $stmt = $pdo->prepare($query);
+    $stmt->execute();
+?>
